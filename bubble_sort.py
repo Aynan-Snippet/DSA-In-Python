d2b1705f12ab0a8e4bc122ac486909b246b30079
Aynan <aynan@gmail.com>
2026-09-01 21:30:00 +0500

    Added bubble_sort algorithm

---
+++ bubble_sort.py
@@ -0,0 +1,8 @@
+# Bubble Sort is an algorithm that sorts an array from the lowest value to the highest value.
+my_array=[33, 27, 9, 10, 56, 69]
+n=len(my_array)
+for i in range(n-1):
+    for j in range(n-i-1):
+        if my_array[j] > my_array[j+1]:
+            my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+print("The sorted array is:", my_array)
