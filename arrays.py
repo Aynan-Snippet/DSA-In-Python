4d43104f22326b4f90d38e985945e32192a3254d
Aynan <aynan@gmail.com>
2026-09-01 21:29:08 +0500

    Added the method for finding maximum_val in array

--- arrays.py
+++ arrays.py
@@ -8,3 +8,6 @@ for i in my_array:
 print("The lowest value in the array is:", min_val)
 
 #Time Complexity: O(n) - The algorithm iterates through the entire array once, making it linear in time complexity. 
+  
+  #Similarly, we can find the highest value in an array using a similar approach.
+  #by just changing the comparison operator from '<' to '>' in the if statement.
